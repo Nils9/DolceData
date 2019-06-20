703981db12aea48c68d42c18927ccd19eea54045
a.ndoko <arthur.ndoko@telecom-paristech.fr>
2019-06-20 17:46:19 +0200

    Ajout code Julien H.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-
+}
 const w = 500;
 const h = 500;
 const padX = 30;
@@ -109,9 +108,11 @@ d3.csv("data/film.csv")
   x = d3.scaleLinear().domain(d3.extent(rows, (row) => row.long)).range([0, w]);
   y = d3.scaleLinear().domain(d3.extent(rows, (row) => row.lat)).range([h, 0]);
   dataset = rows;
-  let ff = yearFilter(dataset, 1975, 1978);
+  let ff = yearFilter(dataset, 1950, 1995);
   let popu = getPopularity(ff, "Redford - Robert");
   let award = getNbOfAwards(ff, "Redford - Robert");
+  let sixSubjects = getSixSubjects(ff, "Redford - Robert");
+  let director = favoriteDirector(ff, "Redford - Robert");
   //draw();
 });
 
@@ -122,7 +123,7 @@ function yearFilter(films, date_min, date_max) {
     let myear = films[i].year;
     if ((myear<date_max) && (myear>date_min)) {
       set.push(films[i]);
-      console.log(films[i]); 
+      console.log(films[i]);
     }
   }
   console.log("-> Total: " + set.length + " films");
@@ -137,7 +138,7 @@ function popularityFilter(films, pop_min, pop_max) {
     if ((mpop<date_max) && (mpop>date_min)) {
       set.push(films[i]);
       console.log(films[i]);
-       
+
     }
   }
   console.log("-> Total: " + set.length + " films");
@@ -145,25 +146,76 @@ function popularityFilter(films, pop_min, pop_max) {
 }
 
 
-function getPopularity(films, actor) {
+function getPopularity(films, person) {
 
   var popu = 0;
   for(var i = 0; i<films.length; i++) {
-    if (films[i].actor == actor) {
+    if ((films[i].actor ==  person)||(films[i].actress ==  person)||(films[i].director ==  person)) {
       popu = popu + films[i].popularity;
     }
   }
-  console.log("Popularity of "+ actor +" : " + popu);
+  console.log("Popularity of "+  person +" : " + popu);
   return popu;
 }
 
-function getNbOfAwards(films, actor) {
+
+function getNbOfAwards(films, person) {
   var n = 0;
   for(var i = 0; i<films.length; i++) {
-    if ((films[i].actor == actor) && (films[i].award == "Yes")){
+    if (((films[i].actor ==  person)||(films[i].actress ==  person)||(films[i].director ==  person)) && (films[i].award == "Yes")){
       n = n + 1;
     }
   }
-  console.log("Number of Awards "+ actor +" : " + n);
+  console.log("Number of Awards "+ person +" : " + n);
   return n;
+}
+
+function sort_object(obj) {
+    items = Object.keys(obj).map(function(key) {
+        return [key, obj[key]];
+    });
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+    return(items)
+}
+
+function getFilmsPerSubject(films, person){
+  var filmsPerSubject = {"Action":0, "Adventure":0, "Comedy":0, "Crime":0, "Drama": 0, "Fantasy": 0, "Horror": 0, "Music": 0, "Mystery": 0, "Romance": 0, "Science-Fiction": 0, "Short": 0, "War": 0, "Western": 0, "Westerns": 0};
+  for(var i = 0; i<films.length; i++) {
+    if (((films[i].actor ==  person)||(films[i].actress ==  person)||(films[i].director ==  person))) {
+      filmsPerSubject[films[i].subject] = filmsPerSubject[films[i].subject]+1;
+    }
+  }
+  return filmsPerSubject;
+}
+
+function getSixSubjects(films, person){
+  var dict = getFilmsPerSubject(films, person);
+  var filmsPerSubject = sort_object(dict);
+  console.log(filmsPerSubject.slice(0,6));
+  return filmsPerSubject.slice(0,6);
+
+}
+
+function getFavoriteSubject(films, person){
+  var dict = getFilmsPerSubject(films, person)
+  var filmsPerSubject = sort_object(dict);
+  return filmsPerSubject[0][0];
+}
+
+function favoriteDirector(films, person){
+  var directors = {};
+  for(var i = 0; i<films.length; i++) {
+    if ((films[i].actor ==  person)||(films[i].actress ==  person)) {
+      if(films[i].director in directors){
+        directors[films[i].director] = directors[films[i].director]+1;
+      }
+      else{
+        directors[films[i].director] = 1;
+      }
+    }
+  }
+  sortedDirectors = sort_object(directors);
+  return sortedDirectors[0][0];
 }
